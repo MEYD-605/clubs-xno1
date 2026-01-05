32feb587dfdacf03126c07f76012956de7d0352f
No.1 Unified Oracle <ai.no.1bro@gmail.com>
2026-01-06 00:04:59 +0700

    chore: cleanup gitignore and remove dist from index

--- dist/_worker.js/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { p as page } from '../chunks/generic_w6xbsvA_.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers__-iShZub.mjs';
